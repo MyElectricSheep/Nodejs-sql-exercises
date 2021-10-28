9a7772a046f900f15cea426798a3c37edca3b29b
Ben Paget <ben.paget@live.fr>
2021-10-28 14:28:24 +0100

    Application level middleware

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.use(express.json());
 
 // Uncomment next line to apply the "secure"
 // middleware to all incoming requests
-app.use(secure);
+// You'll have to provide a token as a query string parameter
+// to access any further route (eg: localhost:3000/api/endpoint?token=something)
+// app.use(secure);
 
 app.use("/api/users", users);
 app.use("/api/orders", orders);
